b2539e1edd4ee5824a090b37fbeafc49de80b58e
miguelAngelo1999 <miguelAngelo1999@users.noreply.github.com>
2026-06-06 10:56:49 -0300

    fix: return real isAdmin value, let web UI handle display as toast

--- api/routes/is_elevated.go
+++ api/routes/is_elevated.go
@@ -1,23 +1,23 @@
 package routes
 
 import (
-	"net/http"
+"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
-	"github.com/igoogolx/itun2socks/pkg/is_elevated"
+"github.com/go-chi/chi/v5"
+"github.com/go-chi/render"
+"github.com/igoogolx/itun2socks/pkg/is_elevated"
 )
 
 func isAdminRouter() http.Handler {
-	r := chi.NewRouter()
-	r.Get("/", getIsAdmin)
-	return r
+r := chi.NewRouter()
+r.Get("/", getIsAdmin)
+return r
 }
 
 var IsElevated = is_elevated.Get()
 
 func getIsAdmin(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, r, render.M{
-		"isAdmin": IsElevated,
-	})
+render.JSON(w, r, render.M{
+"isAdmin": IsElevated,
+})
 }
